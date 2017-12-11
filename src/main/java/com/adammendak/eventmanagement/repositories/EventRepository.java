d35bbb546a1eb4738560a9d993bcc3c945bf293a
adammendak <adam.mendak@gmail.com>
2017-12-11 10:14:40 +0100

    security

--- src/main/java/com/adammendak/eventmanagement/repositories/EventRepository.java
+++ src/main/java/com/adammendak/eventmanagement/repositories/EventRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZoneId;
-import java.util.List;
 
 @Repository
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
@@ -16,4 +16,8 @@ public interface EventRepository extends PagingAndSortingRepository<Event, Long>
     Page<Event> findByName (@Param("name") String name, Pageable pageable);
 
     Page<Event> findByNameAndZoneId(@Param("name")String name, @Param("zoneId")ZoneId zoneId, Pageable pageable);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Long id);
 }
