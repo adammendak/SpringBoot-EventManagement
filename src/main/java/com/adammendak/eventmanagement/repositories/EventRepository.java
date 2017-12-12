dcad7ee52f570e9d4e8f12176c595288c4ce7ce0
adammendak <adam.mendak@gmail.com>
2017-12-12 07:42:14 +0100

    rest resource projection

--- src/main/java/com/adammendak/eventmanagement/repositories/EventRepository.java
+++ src/main/java/com/adammendak/eventmanagement/repositories/EventRepository.java
@@ -1,16 +1,17 @@
 package com.adammendak.eventmanagement.repositories;
 
 import com.adammendak.eventmanagement.model.Event;
+import com.adammendak.eventmanagement.model.PartialEventProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Repository;
 
 import java.time.ZoneId;
 
-@Repository
+@RepositoryRestResource(excerptProjection = PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
     Page<Event> findByName (@Param("name") String name, Pageable pageable);
